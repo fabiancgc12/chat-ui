1fc26eaf7d5b7142bb568df788cd832d6db23c8e
fabian graterol <fabiancgraterol@gmail.com>
2022-12-09 11:39:56 -0400

    creating logout button

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,16 +3,20 @@ import {Button, Flex, Header, Title} from "@mantine/core";
 import {useSocket} from "@/global/socketContext";
 import {useAtom, useSetAtom} from "jotai";
 import {userAtom} from "@/global/user.atom";
+import {RESET} from "jotai/utils";
+import {usersAtom} from "@/global/users.atom";
 
 export function AppHeader(){
     const socket = useSocket()
     const setUser = useSetAtom(userAtom)
+    const setUsersList = useSetAtom(usersAtom)
     return (
         <Header height={70} p={"xs"} sx={{display:"flex",alignItems:"center"}}>
             <Flex align={"center"} justify={"space-between"} w={"100%"}>
                 <Title order={2} size={"h2"} >Chatify</Title>
                 <Button color="red" onClick={() => {
-                    setUser(undefined)
+                    setUser(RESET)
+                    setUsersList([])
                     socket.disconnect()
                 }}>
                     LogOut
